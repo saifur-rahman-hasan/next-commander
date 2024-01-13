23ad191b1a65cbb9785b3beb43fb4fb013e0a2bc
Saifur Rahman <saifur.dohs@gmail.com>
2024-01-13 19:31:09 +0600

    NextCommandInit Command updated

--- dist/commands/NextCommanderInit.mjs
+++ dist/commands/NextCommanderInit.mjs
@@ -2,27 +2,62 @@ import fs from 'fs';
 import path from 'path';
 
 export default class NextCommanderInit {
-    constructor(){}
+    constructor(){
+        this.destinationPath = path.join(process.cwd(), 'src', 'core');
+    }
 
-    run() {
-        const coreDirectoryPath = path.join(process.cwd(), 'dist/InputFiles/core');
-        console.log(`coreDirectoryPath`, coreDirectoryPath)
-        fs.readdir(coreDirectoryPath, (err, files) => {
-            if (err) {
-                console.error(`Error reading directory: ${err}`);
-                return;
-            }
+    async run() {
+        const owner = 'saifur-rahman-hasan';
+        const repo = 'next-commander';
+        const pathInRepo = 'dist/InputFiles/core';
 
-            if (files.length === 0) {
-                console.log('The "core" directory is empty.');
+        const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${pathInRepo}`;
+
+        try {
+            const response = await fetch(apiUrl);``
+            const data = await response.json();
+            
+            
+            if (Array.isArray(data)) {
+                this.createDirectoryIfNotExists(this.destinationPath);
+
+                for (const item of data) {
+                    if (item.type === 'file') {
+                        await this.downloadAndSaveFile(item.download_url, item.name);
+                        console.log(`Downloaded: ${item.name}`);
+                    }
+                }
             } else {
-                console.log(`Files and directories in the "core" directory:`);
-                files.forEach((file) => {
-                    console.log(file);
-                });
+                console.error(`Error fetching data from GitHub API: ${JSON.stringify(data)}`);
             }
-        });
+        } catch (error) {
+            console.error(`Error fetching data from GitHub API: ${error}`);
+        }
+    }
 
-        console.log('Executing Command:' + this.constructor.name)
+    async downloadAndSaveFile(url, fileName) {
+        if (!url || !fileName) {
+            console.error(`Error: Invalid url or fileName`);
+            return;
+        }
+
+        const response = await fetch(url);
+        const content = await response.text();
+
+        // const filePath = path.join(process.cwd(), 'src', 'core', fileName);
+        const newFileName = `${path.basename(fileName, path.extname(fileName))}.ts`;
+        const filePath = path.join(this.destinationPath, newFileName);
+
+        try {
+            fs.writeFileSync(filePath, content);
+        } catch (error) {
+            console.error(`Error writing file ${fileName}: ${error}`);
+        }
+    }
+
+    createDirectoryIfNotExists(directoryPath) {
+        if (!fs.existsSync(directoryPath)) {
+            fs.mkdirSync(directoryPath, { recursive: true });
+        }
     }
 }
